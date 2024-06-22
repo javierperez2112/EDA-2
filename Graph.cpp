831568893b6cf57b5e28ca98d464232b4855a271
javierperez2112 <jperezlosada@itba.edu.ar>
2024-06-22 15:46:11 -0300

    Graph update

--- Graph.cpp
+++ Graph.cpp
@@ -11,15 +11,14 @@ bool Graph::loadPointData(filesystem::path filePath)
 {
     ifstream file;
     file.open(filePath, ifstream::in);
-    if(!file.is_open())
+    if (!file.is_open())
     {
         cout << "Could not open file!" << endl;
         return false;
     }
     // Count width and height.
     string line;
-    while(getline(file, line))
+    while (getline(file, line))
     {
-        
     }
 }
